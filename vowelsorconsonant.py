af0d505b97754931ea2fb96e8522dccd054dbd88
yahiya12 <37495588+yahiya12@users.noreply.github.com>
2019-07-17 10:20:04 +0530

    Create vowelsorconsonant.py

---
+++ vowelsorconsonant.py
@@ -0,0 +1,7 @@
+ch=input("ENTER THE CHARACTER:")
+if (ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u'):
+    print("the char is vowels")
+elif (ch>='a' and ch<='z' or ch>='A' and ch<='Z'):
+    print("the character is consonant")
+else:
+    print("it not a char")
